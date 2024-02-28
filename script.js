b7fb206b4862eac25587c1b261c61442c431892b
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-28 15:14:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
+//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
+    if (value.length === 1 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    } else if (value.length === 0 && index > 0) {
+      inputs[index - 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace' && index > 0) {
+      inputs[index - 1].focus();
+    }
+  });
 });
