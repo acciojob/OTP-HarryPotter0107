d9a33641c29eefe129abea1451db0583a652c73d
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-28 12:18:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].addEventListener('input', function() {
+                if (this.value.length === 1 && i < inputs.length - 1) {
+                    inputs[i + 1].focus();
+                }
+            });
+
+            inputs[i].addEventListener('keydown', function(event) {
+                if (event.key === 'Backspace' && this.value.length === 0 && i > 0) {
+                    inputs[i - 1].focus();
+                }
+            });
+        }
